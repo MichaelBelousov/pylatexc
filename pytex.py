d1c2b171c420da77f4872ba0019f650ebc8e430c
Michael Belousov <michael.belousov98@gmail.com>
2019-03-22 02:01:06 -0400

    started pytex grammar

--- pytex.py
+++ pytex.py
@@ -10,30 +10,89 @@ to formatted values.
 
 import sys
 import os
-sys.path.insert(0, '../../')  # this is blasphemous
-# from pyparsing import *  # I'm sorry PEP for I have sinned
 import re
 from textwrap import dedent
+from pyparsing import *
 
-scope = {}
+##### Parser #####
 
-src = sys.stdin.read()
+LBRACE = '{'
+RBRACE = '}'
+NOTBRACE = CharsNotIn(LBRACE + RBRACE)
 
-# naive pyexec
-def pyexec(match):
-    code = dedent(match[1])
-    exec(code, scope)
-    return ''
+SINGLE_QUOTE = "'"
+DOUBLE_QUOTE = '"'
+REGULAR_QUOTE = Literal(SINGLE_QUOTE) | Literal(DOUBLE_QUOTE)
+TRIPLE_SINGLE_QUOTE = "'''"
+TRIPLE_DOUBLE_QUOTE = '"""'
+TRIPLE_QUOTE = Literal(TRIPLE_SINGLE_QUOTE) | Literal(TRIPLE_DOUBLE_QUOTE)
 
-src = re.sub(r'\\begin\{pyexec\}((.|\n)*?)\\end\{pyexec\}',
-             pyexec, src, flags=re.MULTILINE)
+UNESCAPED_REGULAR_QUOTE = PrecededBy(CharsNotIn('\\')) + REGULAR_QUOTE
+UNESCAPED_TRIPLE_QUOTE = PrecededBy(CharsNotIn('\\')) + TRIPLE_QUOTE
 
-# naive escaped pyeval
-def pyeval(match):
-    code = match[1].replace(r'\}','}')
-    return str(eval(code, scope))
+UNESCAPED_QUOTE = ( UNESCAPED_TRIPLE_QUOTE 
+                  | UNESCAPED_REGULAR_QUOTE )
+NOTQUOTE = CharsNotIn(SINGLE_QUOTE + DOUBLE_QUOTE)
 
-src = re.sub(r'\\pyeval\{(.*?)(?<!\\)\}',
-             pyeval, src, flags=re.MULTILINE)
+p_pyexec_start = Literal(r'\begin{pyexec}')
+p_pyexec_end   = Literal(r'\end{pyexec}')
+p_pyeval_start = Literal(r'\pyeval')
 
-print(src)
+p_closed_brace_text = Forward()
+p_closed_brace_text << ( ZeroOrMore( Optional(NOTBRACE)
+                                    + LBRACE
+                                    + p_closed_brace_text
+                                    + RBRACE )
+                        + Optional(NOTBRACE) )
+p_closed_brace_text = Combine(p_closed_brace_text)
+
+
+# need to add support for single quote and triple quote recognition, as well
+# as escaped quote recognition
+p_even_quote_amt = ( ZeroOrMore( Optional(NOTQUOTE)
+                               + UNESCAPED_QUOTE
+                               + Optional(NOTQUOTE)
+                               + UNESCAPED_QUOTE )
+                   + NOTQUOTE )
+
+# NOTQUOTED(py_exec
+# Combine(
+p_pyexec = ( p_pyexec_start
+           + Combine(Optional( p_even_quote_amt
+                             + p_pyexec_end
+                             + p_even_quote_amt ))       ('value')
+           + p_pyexec_end )
+
+
+p_pyeval = ( p_pyeval_start
+           + LBRACE
+           + Combine(p_closed_brace_text)                ('value')
+           + RBRACE )
+
+
+def main():
+
+    src = sys.stdin.read()
+    scope = {}
+
+    # naive pyexec
+    def pyexec(match):
+        code = dedent(match[1])
+        exec(code, scope)
+        return ''
+
+    src = re.sub(r'\\begin\{pyexec\}((.|\n)*?)\\end\{pyexec\}',
+                 pyexec, src, flags=re.MULTILINE)
+
+    # naive escaped pyeval
+    def pyeval(match):
+        code = match[1].replace(r'\}','}')
+        return str(eval(code, scope))
+
+    src = re.sub(r'\\pyeval\{(.*?)(?<!\\)\}',
+                 pyeval, src, flags=re.MULTILINE)
+
+    print(src)
+
+if __name__ == '__main__':
+    main()
