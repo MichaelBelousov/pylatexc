f140ea25feea925a7f20f1c82a4b0bfdf8788591
Michael Belousov <michael.belousov98@gmail.com>
2019-04-02 12:37:42 -0400

    fleshed out setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 """
-setup for pytex package,
-a package for embedding python in latex documents
+setup for the pytexc package
+a package for embedding python code in latex documents
 """
 
+from setuptools import setup, find_packages
+
+__version__ = '0.1.0'
 __author__ = 'Michael Belousov'
 
-import setuptools
+setup(name='pytexc',
+      version=__version__,
+      author=__author__,
+      description='A compiler of pytex files into normal tex files',
+      url='',
+      packages=find_packages(),
+      install_requires=[ 'six' ])
